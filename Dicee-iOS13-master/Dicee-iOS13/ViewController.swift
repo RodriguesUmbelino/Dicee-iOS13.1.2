768976ce9c6c3c07448d5cce7146aa3336a1d013
Pablo Rodrigues S. J. Umbelino <42478580+RodriguesUmbelino@users.noreply.github.com>
2020-06-26 21:41:01 -0700

    Add files via upload

---
+++ Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var deciImageVeiw1: UIImageView!
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let dadoArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+ 
+            deciImageVeiw1.image = dadoArray[Int.random(in: 0...5)]
+        
+            diceImageView1.image = dadoArray[Int.random(in: 0...5)]
+        
+         
+        
+    }
+    
+}
+
